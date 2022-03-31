e10cb30f3bf8e4ae6bb64f143391227a7ea5771c
a-mikhaylov <sasha.mikhailov99.com@gmail.com>
2022-03-31 03:49:05 +0300

    Сложение матриц

--- 3/multimatrix.ts
+++ 3/multimatrix.ts
@@ -1,24 +1,29 @@
-const matrixA = [[1,2,3], [1,2,3], [1,2,3]]
-const matrixB = [[1,1,1], [1,1,1], [1,1,1]]
+const matrixA = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+const matrixB = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
 function multiplyMatrix(A: number[][], B: number[][]): false | any[][] {
-    let rowsA = A.length, colsA = A[0].length,
-        rowsB = B.length, colsB = B[0].length,
-        C = [];
-    if (colsA != rowsB) {
+    let rowA: number = A.length, colA: number = A[0].length,
+        rowB: number = B.length, colB: number = B[0].length,
+        C = []
+
+    if (colA !== rowB) {
         return false
     }
-    for (let i = 0; i < rowsA; i++) {
-        C[i] = [];
+
+    for (let i = 0; i < rowA; i++) {
+        C[i] = []
     }
-    for (let k = 0; k < colsB; k++) {
-        for (let i = 0; i < rowsA; i++) {
+
+    for (let k = 0; k < colB; k++) {
+        for (let i = 0; i < rowA; i++) {
             let temp = 0
-            for (let j = 0; j < rowsB; j++) temp += A[i][j] * B[j][k];
+            for (let j = 0; j < rowB; j++) {
+                temp += A[i][j] * B[j][k]
+            }
             C[i][k] = temp
         }
     }
-    return C;
+    return C
 }
 
 console.log(multiplyMatrix(matrixA, matrixB))
